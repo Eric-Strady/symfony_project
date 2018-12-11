a46f9dea2755d7734f319d0b9e3c27cecee96828
Eric-Strady <strady60@gmail.com>
2018-12-11 15:17:15 +0100

    create new twig template for forms

---
+++ src/Louvre/BookingBundle/Form/BuyerType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Louvre\BookingBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class BuyerType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date', DateType::class, array('format' => 'dd / MM / yyyy', 'data' => new \DateTime()))
+            ->add('email', EmailType::class)
+            ->add('quantity', IntegerType::class)
+            ->add('ajouter', SubmitType::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Louvre\BookingBundle\Entity\Buyer'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'louvre_bookingbundle_buyer';
+    }
+
+
+}
