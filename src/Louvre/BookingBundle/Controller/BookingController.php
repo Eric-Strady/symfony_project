96bbeff1a9945a08f8c1e3a34accdb2d810ddc09
Eric-Strady <strady60@gmail.com>
2018-12-11 12:15:06 +0100

    create a form for booking object

--- src/Louvre/BookingBundle/Controller/BookingController.php
+++ src/Louvre/BookingBundle/Controller/BookingController.php
@@ -3,11 +3,29 @@
 namespace Louvre\BookingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Louvre\BookingBundle\Entity\Booking;
+use Louvre\BookingBundle\Form\BookingType;
 
 class BookingController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('@LouvreBooking/Booking/index.html.twig');
+    	$booking = new Booking();
+
+    	$form = $this->createForm(BookingType::class, $booking);
+
+    	if ($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+    	{
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($booking);
+			$em->flush();
+
+			$request->getSession()->getFlashBag()->add('booked', 'Votre réservation a bien été prise en compte !');
+
+			return $this->redirectToRoute('louvre_booking_homepage');
+    	}
+
+        return $this->render('@LouvreBooking/Booking/index.html.twig', array('form' => $form->createView()));
     }
 }
