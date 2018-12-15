0b3635127f245035dc19a9a757ee5dc3b9f5c3d4
Eric-Strady <strady60@gmail.com>
2018-12-15 17:13:11 +0100

    created booking.js

---
+++ web/booking.js
@@ -0,0 +1,10 @@
+$(function(){
+    var container = $('div#louvre_bookingbundle_buyer_bookings');
+    var count = container.find(':input').length;
+    
+    $('#validate_step').click(function(){
+        addBookingForm();
+    });
+
+    function addBookingForm()
+});
