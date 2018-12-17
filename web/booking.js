69e9f8fd3710c28e7dfe28f45d8f574294fb55e3
Eric-Strady <strady60@gmail.com>
2018-12-18 00:49:57 +0100

    add event for form

--- web/booking.js
+++ web/booking.js
@@ -1,10 +1,42 @@
 $(function(){
-    var container = $('div#louvre_bookingbundle_buyer_bookings');
-    var count = container.find(':input').length;
-    
-    $('#validate_step').click(function(){
-        addBookingForm();
+	$('#step4').hide();
+	$('#step5').hide();
+
+    $('#visitorForm').click(function()
+    {
+        var nbVisitors = $('#louvre_bookingbundle_buyer_quantity').val();
+
+        if (nbVisitors > 0 && nbVisitors < 30)
+        {
+            $('#step4').fadeIn('slow');
+            var container = $('div#louvre_bookingbundle_buyer_bookings');
+            var count = container.find(':input').length;
+
+            addVisitorForm(container, nbVisitors);
+        }
+        else
+        {
+            alert('Vous devez sasir un nombre de visiteurs entre 0 et 30 !');
+        }    	
     });
 
-    function addBookingForm()
+    function addVisitorForm(container, nbVisitors)
+    {
+        for (var i = 1; i <= nbVisitors; i++)
+        {
+            var builder = container.attr('data-prototype')
+                .replace(/__name__label__/g, 'Visiteur n°' + (i))
+                .replace(/__name__/g, i);
+
+            var visitorForm = $(builder);
+
+            container.append(visitorForm);
+        } 
+    }
+
+    $('#emailForm').click(function()
+    {
+        $('#step5').fadeIn('slow');      
+    });
+
 });
