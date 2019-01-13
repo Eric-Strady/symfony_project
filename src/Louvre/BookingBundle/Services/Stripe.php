483f0dd31e1b566165b7745d5e392684cfdf4b15
Eric-Strady <strady60@gmail.com>
2019-01-13 17:08:55 +0100

    payment worked

--- src/Louvre/BookingBundle/Services/Stripe.php
+++ src/Louvre/BookingBundle/Services/Stripe.php
@@ -2,17 +2,20 @@
 
 namespace Louvre\BookingBundle\Services;
 
+use Louvre\BookingBundle\Entity\Buyer;
+
 class Stripe
 {
-
 	public function payment(Buyer $buyer)
 	{
 		\Stripe\Stripe::setApiKey("sk_test_U7Dow6nqjUiYNLUGdEwV8W0O");
 
-		$token = '?';
+		$amount = $buyer->getTotalPrice();
+		$quantity = $buyer->getQuantity();
+		$token = $_POST['stripeToken'];
 
 		$charge = \Stripe\Charge::create([
-		    'amount' => $amount,
+		    'amount' => $amount * 100,
 		    'currency' => 'eur',
 		    'description' => 'Réservation pour ' . $quantity . ' personnes',
 		    'source' => $token,
