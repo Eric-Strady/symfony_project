eb0c993c0e2848ea40c0f25ec3616de94188b0f1
Eric-Strady <strady60@gmail.com>
2019-01-14 14:17:06 +0100

    change text button and style

--- web/checkout.js
+++ web/checkout.js
@@ -1,4 +1,6 @@
 $(function(){ 
     // STRIPE
-    
+    $('.stripe-button-el').attr('id', 'stripeButton');
+    $('#stripeButton').attr('class', 'btn btn-primary');
+    $('#stripeButton span').replaceWith('Payer ma réservation');
 });
