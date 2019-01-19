d1bc5dfe2db6a8043795d7b785ee461bc7a13068
Eric-Strady <strady60@gmail.com>
2019-01-19 16:07:34 +0100

    change home page style + add smooth scroll anchor

--- web/booking.js
+++ web/booking.js
@@ -1,4 +1,16 @@
 $(function(){
+
+    $('.anchor').click(function(e) {
+        if (this.hash !== "")
+        {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){
+            window.location.hash = hash;
+            });
+        }
+    });
+
 	$('#step4').hide();
 	$('#step5').hide();
 
