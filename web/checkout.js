7ee367818cf1ec31e403ea9362e16bd532340229
Eric-Strady <strady60@gmail.com>
2019-01-27 02:59:02 +0100

    move css and js files in new folders and changed links

--- web/checkout.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$('footer').attr('class', 'footer fixed-bottom');
-
-    // STRIPE
-    $('.stripe-button-el').attr('id', 'stripeButton');
-    $('#stripeButton').attr('class', 'btn btn-primary');
-    $('#stripeButton span').replaceWith('Payer ma réservation');
-});
