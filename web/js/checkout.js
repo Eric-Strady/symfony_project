150d173248f6b6ac6ca1f93021e0641c45107e6b
Eric-Strady <strady60@gmail.com>
2019-01-27 04:21:31 +0100

    worked on checkout page style

--- web/js/checkout.js
+++ web/js/checkout.js
@@ -2,6 +2,14 @@ $(function(){
 	$('footer').attr('class', 'footer fixed-bottom');
 
     // STRIPE
+    $('#payment').hide();
+
+    $('#confirmed').click(function() {
+    	$('#decision').hide();
+    	$('.icons').css('color', '#43a526');
+		$('#payment').fadeIn('slow');
+	});
+
     $('.stripe-button-el').attr('id', 'stripeButton');
     $('#stripeButton').attr('class', 'btn btn-primary');
     $('#stripeButton span').replaceWith('Payer ma réservation');
