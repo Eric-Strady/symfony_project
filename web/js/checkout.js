19ec4f046215e109857bf52a66c6e24eececdf82
Eric-Strady <strady60@gmail.com>
2019-02-01 21:13:42 +0100

    error message if total price = 0

--- web/js/checkout.js
+++ web/js/checkout.js
@@ -1,6 +1,15 @@
 $(function(){
 	$('footer').attr('class', 'footer fixed-bottom');
 
+    // TOTAL PRICE = 0
+    if (totalPrice === 0)
+    {
+        var message = '<span class="col-lg-12 alert alert-danger">Vous ne pouvez pas effectuer de réservation pour un montant de 0,00€.</span>';
+        $('.icons:last').css('color', 'red');
+        $('#confirmed').attr('class', 'btn btn-light btn-lg rounded-pill disabled');
+        $('#orderSummary').append(message);
+    }
+
     // STRIPE
     $('#payment').hide();
 
